07d2f97ac4353701a20941e6a4f397fdbe257d29
jroper1130 <warbeast78@hotmail.com>
2017-11-03 14:44:01 -0700

    Add files via upload

---
+++ ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeForXmenBattleCount.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        &nbsp;<asp:Label ID="Label1" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeForXmenBattleCount
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //arrays
+            string[] names = new string[] {"X", "iceman", "angel", "beast", "phoenix", "cyclops", "wolverine", "nightcrawler", "rogue", "storm"};
+            int[] numbers = new int[] {7, 9, 12, 15, 17, 13, 2, 6, 8, 13};
+
+            //initialize variables
+            string result = "";
+
+            int largestNumberIndex = 0;
+            int smallestNumberIndex = 0;
+
+            //loop iteration logic 
+            for (int i = 0; i < names.Length; i++)
+            {
+                //check for largest
+                if (numbers[i] > numbers[largestNumberIndex])
+                {
+                    largestNumberIndex = i;
+                }
+                //check for smallest
+                if (numbers[i] < numbers[smallestNumberIndex])
+                {
+                    smallestNumberIndex = i;
+                }
+            }
+            //format variables into strings for lable using their indeces
+            result = String.Format("Most battles belong to: {0} (Value: {1})", names[largestNumberIndex], numbers[largestNumberIndex]);
+            result += String.Format("<br/>Least battles belong to: {0} (Value: {1})", names[smallestNumberIndex], numbers[smallestNumberIndex]);
+
+            //print out results
+            Label1.Text = result;
+        }
+    }
+}
